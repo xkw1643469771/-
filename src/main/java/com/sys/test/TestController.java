11351a634dd26b504d7d904407b2196c8f64a47c
xumou <xumou@xumou-PC>
2018-12-09 19:20:08 +0800

    12

--- src/main/java/com/sys/test/TestController.java
+++ src/main/java/com/sys/test/TestController.java
@@ -1,15 +1,26 @@
 package com.sys.test;
 
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-	
-	@RequestMapping("test")
+
+	@RequestMapping("/")
 	public String test() {
-		System.out.println("12312321313");
-		return "";
+		return "/html/index.html";
 	}
 
+	@RequestMapping("/goto/**")
+	public String test2(HttpServletRequest request) {
+		String uri = request.getRequestURI();
+		if (Pattern.matches("/goto[/]{0,1}", uri)) {
+			return "/html/index.html";
+		}
+		return "/html/" + uri.substring(6) + ".html";
+	}
 }
