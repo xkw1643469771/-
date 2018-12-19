ffd18f8cc162fb7793a141006cb7e693c24bfac5
xumou <1643469771@qq.com>
2018-12-19 09:57:10 +0800

    20181218

--- src/test/java/com/test/mybatis/generator/custom/PropertiesPlugin.java
+++ src/test/java/com/test/mybatis/generator/custom/PropertiesPlugin.java
@@ -1,7 +1,22 @@
 package com.test.mybatis.generator.custom;
 
+import org.mybatis.generator.api.ConnectionFactory;
+import org.mybatis.generator.api.PluginAdapter;
+
+import java.util.List;
+import java.util.Properties;
+
 /**
  *
  */
-public class PropertiesPlugin {
+public class PropertiesPlugin extends PluginAdapter {
+    @Override
+    public boolean validate(List<String> warnings) {
+        System.out.println(warnings);
+        return true;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+    }
 }
